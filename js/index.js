f2b8e6705efdf25f702920e2eb207552a8d59e3f
brjest85 <38208585+brjest85@users.noreply.github.com>
2022-06-21 21:40:53 +0100

    Added website

---
+++ js/index.js
@@ -0,0 +1,18 @@
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById('logo'), // Required
+  path: 'json/data.json', // Required
+  renderer: 'svg', // Required
+  loop: true, // Optional
+  autoplay: true, // Optional
+  name: "Hello World", // Name for future reference. Optional.
+})
+
+
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById('happyman'), // Required
+  path: 'json/data2.json', // Required
+  renderer: 'svg', // Required
+  loop: true, // Optional
+  autoplay: true, // Optional
+  name: "Hello World", // Name for future reference. Optional.
+})
